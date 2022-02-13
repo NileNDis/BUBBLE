21679ca393b5dff1d03f68dd98fc06f064439611
NileNDis <denis.gg2018@gmail.net>
2022-02-13 10:00:50 +0200

    Add project files.

---
+++ BUBBLE.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+//int main() //Bubble sort
+//{
+//    int* arr; // указатель для выделения памяти под массив
+//    int size; // размер массива
+//
+//    // Ввод количества элементов массива
+//    cout << "n = ";
+//    cin >> size;
+//
+//    if (size < 0) {
+//        // Размер масива должен быть положитлеьным
+//        cout << "Invalid size" << endl;
+//    }
+//
+//    arr = new int[size]; // выделение памяти под массив
+//
+//    // заполнение массива
+//    for (int i = 0; i < size; i++) {
+//        cout << "arr[" << i << "] = ";
+//        cin >> arr[i];
+//    }
+//
+//    int temp; // временная переменная для обмена элементов местами
+//
+//    // Сортировка массива пузырьком
+//    for (int i = 0; i < size - 1; i++) {
+//        for (int j = 0; j < size - i - 1; j++) {
+//            if (arr[j] > arr[j + 1]) {
+//                // меняем элементы местами
+//                temp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = temp;
+//            }
+//        }
+//    }
+//
+//    // Вывод отсортированного массива на экран
+//    for (int i = 0; i < size; i++) {
+//        cout << arr[i] << " ";
+//    }
+//    cout << endl;
+//
+//    delete[] arr; // освобождение памяти;
+//
+//    system("PAUSE");
+//
+//    return 0;
+//}
+
+std::size_t patition()
+{
+
+}
+
+void q_sort(std::vector<int>& vec, std::size_t left, std::size_t right)//Quick sort recurssion 
+{
+
+}
+
+int main() // Quick sort
+{
+    return 0;
+}
